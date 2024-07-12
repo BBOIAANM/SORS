d1639a33d60172ad659b305092cf12e8c6399cd8
BBOIAANM <167621391+BBOIAANM@users.noreply.github.com>
2024-07-12 14:14:12 +0300

    Update search.py

--- FallenMusic/Modules/search.py
+++ FallenMusic/Modules/search.py
@@ -35,9 +35,9 @@ async def ytsearch(_, message: Message):
         pass
     try:
         if len(message.command) < 2:
-            return await message.reply_text("⎊ اكتب اللى عايز تبحث عنه 🙃")
+            return await message.reply_text("⎊ اكتب الي تريد تبحث عنة يخالي 🙂")
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("⎊ جارٍ البحث...")
+        m = await message.reply_text("💁 اصبر جاي ابحث...")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
